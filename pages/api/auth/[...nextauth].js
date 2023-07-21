a493743b964ad29d25902ead684882238ac87ce4
Bruno Cicerchia <brunocicerchia.dev@gmail.com>
2023-07-21 04:28:17 -0300

    Adapter Error solucionado <3
    Despues de semanas logre solucionar el error y lo peor es que era una boludez xdxd anashe

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,18 +1,36 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "./lib/mongodb"
 
-export const authOptions = {
-  // Configure one or more authentication providers
+//export const authOptions = {
+//  // Configure one or more authentication providers
+//  providers: [
+//    GithubProvider({
+//      adapter: MongoDBAdapter(clientPromise),
+//      clientId: process.env.GITHUB_ID,
+//      clientSecret: process.env.GITHUB_SECRET
+//    }),
+//    GoogleProvider({
+//      client_id: process.env.GOOGLE_CLIENT_ID,
+//      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//    })
+//    // ...add more providers here
+//  ],
+//}
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
       adapter: MongoDBAdapter(clientPromise),
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET
     }),
-    // ...add more providers here
-  ],
-}
-
-export default NextAuth(authOptions)
+    GoogleProvider({
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ]
+})
--- pages/api/auth/lib/mongodb.ts
+++ pages/api/auth/lib/mongodb.ts
@@ -17,11 +17,17 @@ if (!process.env.MONGODB_URI) {
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+
+  let globalWithMongoClientPromise = global as typeof globalThis & {
+    _mongoClientPromise: Promise<MongoClient>;
+  };
+  if (!globalWithMongoClientPromise._mongoClientPromise) {
     client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    globalWithMongoClientPromise._mongoClientPromise = client.connect()
   }
-  clientPromise = global._mongoClientPromise
+  console.log(globalWithMongoClientPromise._mongoClientPromise)
+  clientPromise = globalWithMongoClientPromise._mongoClientPromise
+  console.log(globalWithMongoClientPromise._mongoClientPromise)
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options)
